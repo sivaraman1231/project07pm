542800126a93d71f68648b38cd4c17e9ed917a41
sivaraman1231 <srishivaraman@gmail.com>
2022-09-09 12:18:37 +0530

    siva code

--- src/test/java/org/ttest/s/SampleClass.java
+++ src/test/java/org/ttest/s/SampleClass.java
@@ -1,5 +1,13 @@
 package org.ttest.s;
 
 public class SampleClass {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("test1");
+		System.out.println("test2");
+		System.out.println("test3");
+
+	}
 
 }
